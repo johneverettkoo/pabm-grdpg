ade018a1f1246eb4c00e7915fb750f7907ca76eb
John Koo <johnkoo@i6.red.uits.iu.edu>
2020-10-21 10:29:06 -0400

    more simulations

--- rmse-sim-k.R
+++ rmse-sim-k.R
@@ -10,15 +10,16 @@ import::here(embedding, draw.graph, I.pq, generate.P.beta,
              .from = 'functions.R')
 
 # parallel backend
-doMC::registerDoMC(parallel::detectCores())
+doMC::registerDoMC(parallel::detectCores() / 2)
 
 # simulation params
-K.vec <- c(4, 8)
+K.vec <- c(4, 3, 2)
 alpha <- .5
 a1 <- b2 <- 2
 a2 <- b1 <- 1
-n.vec <- c(64, 128, 256, 512, 1024, 2048)
-iter <- 100
+n.vec <- c(128, 256, 512, 1024, 2048, 4096)
+n.vec <- rev(n.vec)
+iter <- 40
 set.seed(314159)
 
 # simulation
@@ -27,7 +28,8 @@ rmse.df <- foreach(K = K.vec, .combine = dplyr::bind_rows) %do% {
   q <- K * (K - 1) / 2
   Ipq <- I.pq(p, q)
   
-  foreach(n = n.vec, .combine = dplyr::bind_rows) %do% {
+  out <- foreach(n = n.vec, .combine = dplyr::bind_rows) %do% {
+    print(paste('n =', n, 'K =', K))
     plyr::ldply(seq(iter), function(i) {
       P.z <- generate.P.beta(n, K)
       P <- P.z$P
@@ -41,31 +43,32 @@ rmse.df <- foreach(K = K.vec, .combine = dplyr::bind_rows) %do% {
         return()
     }, .parallel = TRUE) %>% 
       return()
-  } %>%
-    return()
+  }
+  gc()
+  return(out)
 }
 
-# rmse.df %>% 
-#   na.omit() %>% 
-#   dplyr::group_by(K, n) %>% 
-#   dplyr::summarise(median.rmse = median(rmse),
-#                    q1.rmse = quantile(rmse, .25),
-#                    q3.rmse = quantile(rmse, .75),
-#                    median.rmse.mle = median(rmse.mle),
-#                    q1.rmse.mle = quantile(rmse.mle, .25),
-#                    q3.rmse.mle = quantile(rmse.mle, .75)) %>% 
-#   dplyr::ungroup() %>% 
-#   ggplot() + 
-#   scale_y_log10() + 
-#   scale_x_log10() +
-#   geom_line(aes(x = n, y = median.rmse, colour = 'Algorithm 4')) + 
-#   geom_errorbar(aes(x = n, ymin = q1.rmse, ymax = q3.rmse,
-#                     colour = 'Algorithm 4')) + 
-#   geom_line(aes(x = n, y = median.rmse.mle, colour = 'MLE-based')) + 
-#   geom_errorbar(aes(x = n, ymin = q1.rmse.mle, ymax = q3.rmse.mle,
-#                     colour = 'MLE-based')) + 
-#   scale_colour_brewer(palette = 'Set1') + 
-#   labs(y = 'RMSE', colour = NULL) + 
-#   facet_wrap(~ K)
+rmse.df %>%
+  na.omit() %>%
+  dplyr::group_by(K, n) %>%
+  dplyr::summarise(median.rmse = median(rmse),
+                   q1.rmse = quantile(rmse, .25),
+                   q3.rmse = quantile(rmse, .75),
+                   median.rmse.mle = median(rmse.mle),
+                   q1.rmse.mle = quantile(rmse.mle, .25),
+                   q3.rmse.mle = quantile(rmse.mle, .75)) %>%
+  dplyr::ungroup() %>%
+  ggplot() +
+  scale_y_log10() +
+  scale_x_log10() +
+  geom_line(aes(x = n, y = median.rmse, colour = 'Algorithm 4')) +
+  geom_errorbar(aes(x = n, ymin = q1.rmse, ymax = q3.rmse,
+                    colour = 'Algorithm 4')) +
+  geom_line(aes(x = n, y = median.rmse.mle, colour = 'MLE-based')) +
+  geom_errorbar(aes(x = n, ymin = q1.rmse.mle, ymax = q3.rmse.mle,
+                    colour = 'MLE-based')) +
+  scale_colour_brewer(palette = 'Set1') +
+  labs(y = 'RMSE', colour = NULL) +
+  facet_wrap(~ K)
 
 readr::write_csv(rmse.df, 'rmse-k.csv')
